1b1afcbb44ae3fc6905080af695556bbd62dfb0d
Ivan <vek-2011@mail.ru>
2019-04-15 17:47:53 +0400

    ввод 2 чисел с приглашением

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include<stdio.h>
 #include<locale.h>
 #include<math.h>
+#include<iostream>
+using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	float a, b;
+	cout<<"¬ведите два числа\n";
+	cin>>a>>b;
 	return 0;
 }
